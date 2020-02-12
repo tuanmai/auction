9ea4cfdb7a87dc131934af5fb0b3cc94cccfb70c
Tuan Mai <tuan.mai@employmenthero.com>
2020-02-12 15:29:52 +0700

    Call auction in auction_web

--- apps/auction_web/mix.exs
+++ apps/auction_web/mix.exs
@@ -43,7 +43,9 @@ defmodule AuctionWeb.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:phoenix_ecto, "~> 4.1.0"},
+      {:auction, in_umbrella: true}
     ]
   end
 
