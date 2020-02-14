22a8a4b55896f7e69c02b03852fd4e41d09d7f15
Tuan Mai <tuan.mai@employmenthero.com>
2020-02-14 14:35:42 +0700

    Finish authentication

--- apps/auction_web/lib/auction_web/controllers/user_controller.ex
+++ apps/auction_web/lib/auction_web/controllers/user_controller.ex
@@ -1,5 +1,6 @@
 defmodule AuctionWeb.UserController do
   use AuctionWeb, :controller
+  plug :prevent_unauthorized_access when action in [:show]
 
   def show(conn, %{"id" => id}) do
     user = Auction.get_user(id)
@@ -16,6 +17,23 @@ defmodule AuctionWeb.UserController do
       {:ok, user} -> redirect(conn, to: Routes.user_path(conn, :show, user))
       {:error, user} -> render conn, "new.html", user: user
     end
-    
+  end
+
+  defp prevent_unauthorized_access(conn, _opts) do
+    current_user = Map.get(conn.assigns, :current_user)
+
+    request_user_id =
+      conn.params
+        |> Map.get("id")
+        |> String.to_integer()
+
+    if current_user == nil || current_user.id != request_user_id do
+      conn
+        |> put_flash(:error, "Nice try, friend. That's not your page")
+        |> redirect(to: Routes.item_path(conn, :index))
+        |> halt()
+    else
+      conn
+    end
   end
 end
